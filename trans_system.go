92282fefe9dec11fe6103a54011336916343f9fa
manoty <kevinnoty21@gmail.com>
2025-03-12 17:47:51 +0300

    Added vehicle embedding example

---
+++ trans_system.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+//vehicle struct common
+type Motor struct{
+	Brand string
+	Speed int
+}
+
+// Method for Vehicle
+func (m Motor) Move(){
+	fmt.Println(m.Brand, "is moving at a speed of", m.Speed, "mph")
+}
+
+//car struct embeds motor
+type Car struct{
+	Motor
+	Transmission string
+}
+//scooter struct embedds motor
+type Scooter struct{
+	Motor
+	Gears int
+}
+type Plane struct{
+	Motor 
+	Carrying_capacity int
+}
+
+func main(){
+	car      := Car{Motor: Motor{Brand: "Bugatti cheyron", Speed: 360}, Transmission:"Automatic"}
+	scooter := Scooter{Motor: Motor{Brand:"Honda", Speed: 180}, Gears: 6}
+	plane := Plane{Motor: Motor{Brand:"Emirates", Speed:540}, Carrying_capacity: 300}
+
+	car.Move()
+	scooter.Move()
+	plane.Move()
+
+
+}
